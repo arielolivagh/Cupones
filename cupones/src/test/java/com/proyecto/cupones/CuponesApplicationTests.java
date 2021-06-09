7ca9fb5e27e403544e89ee1ae0da5eea2e29068b
Ariel <Ariel@PCWINDOWS>
2021-06-09 17:36:12 -0500

    Pruebas Mock

--- cupones/src/test/java/com/proyecto/cupones/CuponesApplicationTests.java
+++ cupones/src/test/java/com/proyecto/cupones/CuponesApplicationTests.java
@@ -1,13 +1,23 @@
 package com.proyecto.cupones;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class CuponesApplicationTests {
+	@Autowired
+	MockMvc mock;
 
 	@Test
-	void contextLoads() {
+	void testListausuario() throws Exception {
+		mock.perform(get("/api/usuarios")).andDo(print());
 	}
 
 }
