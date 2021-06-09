a32d6fed8f2de02d11c0b0576ee7b0e296d33048
Ariel <Ariel@PCWINDOWS>
2021-06-09 17:44:08 -0500

    Junit

--- cupones/src/test/java/com/proyecto/cupones/CuponesApplicationTests.java
+++ cupones/src/test/java/com/proyecto/cupones/CuponesApplicationTests.java
@@ -1,12 +1,15 @@
 package com.proyecto.cupones;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureMockMvc
@@ -16,6 +19,24 @@ class CuponesApplicationTests {
 	MockMvc mock;
 
 	@Test
+	@Order(1)
+	void testAltausuario() throws Exception {
+		mock.perform(post("/api/usuario")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\n"
+						+ "        \"nombre\": \"Laura4\",\n"
+						+ "        \"apellido_paterno\": \"Rios\",\n"
+						+ "        \"apellido_materno\": \"Lagos\",\n"
+						+ "        \"correo\": \"laura4@hotmail.com\",\n"
+						+ "        \"contrasena\": \"12345\",\n"
+						+ "        \"referido_por\": \"Juan\",\n"
+						+ "        \"cuota\": 100.0\n"
+						+ "    }")
+				).andDo(print());
+	}
+	
+	@Test
+	@Order(2)
 	void testListausuario() throws Exception {
 		mock.perform(get("/api/usuarios")).andDo(print());
 	}
